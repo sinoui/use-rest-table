e5326bcec64401e57a8cb073af419afcbf5bf9ca
tianyanqiu <tianyanqiu@sinosoft.com.cn>
2019-06-11 11:36:58 +0800

    chore: 抛出类型声明

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import { SortInfo, PageInfo } from '@sinoui/use-rest-page-api';
 import useRestTable from './useRestTable';
+import { DataSource } from './types';
 
 export default useRestTable;
+
+export { SortInfo, PageInfo, DataSource };
